98333806992ade3d652f7980013183769a1a4565
mahruswidip <mahrusrama7@gmail.com>
2026-07-23 11:31:08 +0700

    build gradle

--- rosanatravelapp/android/app/build.gradle.kts
+++ rosanatravelapp/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace ="com.rosanatravel.app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion  = "27.0.12077973"
 
     compileOptions {
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "com.rosanatravel.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
